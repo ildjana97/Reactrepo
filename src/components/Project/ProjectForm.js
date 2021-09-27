026f799e5bfc56111f543502cf60547e75368433
ildjana.mamo <ildjana.mamo@division5.co>
2021-09-27 09:36:02 +0200

    hook_changes

--- src/components/Project/ProjectForm.js
+++ src/components/Project/ProjectForm.js
@@ -1,85 +1,85 @@
-import React, { useState } from 'react';
-import './ProjectForm.css';
-import Card from '../UI/Card';
+import React, { useState } from "react";
+import "./ProjectForm.css";
+import Card from "../UI/Card";
 
 const ProjectForm = () => {
+  const totalAmount = 0.013 * 7.2 * 1000 + 0.015 * 1.14;
+  const [enteredPercentage, setEnteredPercentage] = useState("100");
+  const [enteredYears, setEnteredYears] = useState("1");
+  const [amount, setAmount] = useState("0");
 
-    const AddNumbers = (props) => {
-        const [enteredPercentage, setEnteredPercentage] = useState('');
-        const [enteredYears, setEnteredYears] = useState('');
-    };
-    
+  const percentageChangeHandler = (event) => {
+    setEnteredPercentage(event.target.value);
+    setAmount(((totalAmount * event.target.value) / 100).toFixed(2));
+  };
 
-    const percentageChangeHandler = (event) =>{
-        setEnteredPercentage(event.target.value);
-    };
+  const yearsChangeHandler = (event) => {
+    setEnteredYears(event.target.value);
+    setAmount((totalAmount * event.target.value).toFixed(2));
+  };
 
-    const yearsChangeHandler = (event) =>{
-        setEnteredYears(event.target.value);
-    };
-    
-
-
-    const addNumbersHandler = (event) => {
-        event.preventDefault();
-        if (enteredPercentage < 0){
-            return(Value must be 0 or greater than 0);
-        }
-        console.log( enteredPercentage, enteredYears);
-        setEnteredPercentage('');
-        setEnteredYears('');
-    };
-
-  return <form > 
+  return (
+    <form>
       <Card className="project">
         <div className="new-project-title"> Dein CO2-Ausgleich</div>
         <div className="new-project__values">
-            <div className="new-project__value">
-                <label>%Fubadruck</label>
-                <input type='number'  min="0%" step="10%"  max="100%" className="percentage"
-                value={ } onChange={}/>
-            </div>
+          <div className="new-project__value">
+            <label>%Fubadruck</label>
+            <input
+              type="number"
+              min="0"
+              step="10"
+              max="100"
+              value={enteredPercentage}
+              className="percentage"
+              onChange={percentageChangeHandler}
+            />
+          </div>
 
-            <div className="new-project__value">
-                <label>Anzahl Jahre </label>
-                <input type='number'  min="1" step="1"  className="years"
-                value={ } onChange={}/>
-            </div>
+          <div className="new-project__value">
+            <label>Anzahl Jahre </label>
+            <input
+              type="number"
+              min="1"
+              step="1"
+              className="years"
+              value={enteredYears}
+              onChange={yearsChangeHandler}
+            />
+          </div>
         </div>
 
         <div className="new-project__meassurements">
-            <div className="new-project__meassure">
-                <div className="meassure-name">Ausgleich von</div>
-                <div className="meassure">
-                    <div className="meassure-value">
-                        <div className="meassure-value__value1">7.2t CO2</div>
-                        <img src="" className="logo" alt="logo" />
-                    </div>
-                </div>
+          <div className="new-project__meassure">
+            <div className="meassure-name">Ausgleich von</div>
+            <div className="meassure">
+              <div className="meassure-value">
+                <div className="meassure-value__value1">7.2t CO2💨</div>
+                <img src="" className="logo" alt="logo" />
+              </div>
             </div>
+          </div>
         </div>
 
         <div className="new-project__meassurements separator">
-            <div className="new-project__meassure">
-                <div className="meassure-name">Ausgleich durch</div>
-                <div className="meassure">
-                    <div className="meassure-value">
-                        <div className="meassure-value__value2">1.140l</div>
-                        <img src="" className="logo"   alt="logo" />
-                    </div>
-                </div>
+          <div className="new-project__meassure">
+            <div className="meassure-name">Ausgleich durch</div>
+            <div className="meassure">
+              <div className="meassure-value">
+                <div className="meassure-value__value2">1.140l💧</div>
+                <img src="" className="logo" alt="logo" />
+              </div>
             </div>
-            </div>
-
-            <div className="new-project__betrag">
-            <div className="meassure-name">Betrag</div>
-            <div className="meassure-cash">{}</div>
-            
+          </div>
         </div>
-    </Card>
 
-  </form>
-  
+        <div className="new-project__betrag">
+          <div className="meassure-name">Betrag</div>
+          <div className="meassure-cash">{amount}€</div>
+        </div>
+      </Card>
+    </form>
+  );
 };
 
 export default ProjectForm;
